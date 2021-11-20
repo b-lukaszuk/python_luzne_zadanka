a9bc74ba52ea3eed5c991fb8b55714266cf1c383
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-11-20 10:52:37 +0100

    dodano klase Card (z podstawowa funkcjonalnoscia)

---
+++ part1_18_11_2021/task2/Card.py
@@ -0,0 +1,13 @@
+class Card:
+    """
+    Card with a number 0-99 in it
+    """
+
+    def __init__(self, id: int):
+        if 0 <= id <= 99:
+            self.__id: int = id
+        else:
+            raise IndexError("Id must be between 0 (incl) and 99 (incl)")
+
+    def getId(self) -> int:
+        return self.__id
