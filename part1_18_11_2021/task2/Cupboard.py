689897efec006af733ef97fa794940a6c63b331a
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-11-21 20:33:40 +0100

    kosmetyka, dod. mozliwosc wyboru ilosci kart/szuflad (do testowania)

--- part1_18_11_2021/task2/Cupboard.py
+++ part1_18_11_2021/task2/Cupboard.py
@@ -8,14 +8,17 @@ class Cupboard:
     Cupboard with 100 drawers and 1 Card in each drawer
     """
 
-    def __init__(self):
-        self.__cards: [Card] = [Card(i) for i in range(99)]
+    def __init__(self, noOfDrawersWithCards: int = 100):
+        self.__cards: [Card] = [Card(i) for i in range(noOfDrawersWithCards)]
         random.shuffle(self.__cards)
+        self.__upIndexIncl: int = len(self.__cards) - 1
 
     def getCardIdFromDrawer(self, drawerNo: int) -> int:
-        if 0 <= drawerNo <= 99:
+        if 0 <= drawerNo <= self.__upIndexIncl:
             return self.__cards[drawerNo].getId()
         else:
             raise IndexError(
-                "Drawer number must be between 0 (incl) and 99 (incl)"
+                "Drawer number must be between 0 (incl) and {} (incl)".format(
+                    self.__upIndexIncl
+                )
             )
