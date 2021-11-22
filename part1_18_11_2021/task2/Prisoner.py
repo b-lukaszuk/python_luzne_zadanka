c155535aa2cfa92dfacb542c3476b9063d51870c
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-11-22 10:42:25 +0100

    kosmetyka, wywalenie isLuckyCardFound i setLuckyCardFound z prisonera

--- part1_18_11_2021/task2/Prisoner.py
+++ part1_18_11_2021/task2/Prisoner.py
@@ -18,9 +18,3 @@ class Prisoner:
 
     def getRandInt(self, fromIncl: int = 0, toIncl: int = 99) -> int:
         return random.randint(fromIncl, toIncl)
-
-    def isLuckyCardFound(self) -> bool:
-        return self.__luckyCardFound
-
-    def setLuckyCardFound(self, setTo: bool) -> None:
-        self.__luckyCardFound = setTo
