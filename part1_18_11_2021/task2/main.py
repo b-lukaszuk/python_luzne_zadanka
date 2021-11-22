c3afad40cec07953735dde3cdb806a2865eb1534
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-11-22 10:43:02 +0100

    popr. randomowego wybierania kart przez wiezniow i liczenia prawdop.

--- part1_18_11_2021/task2/main.py
+++ part1_18_11_2021/task2/main.py
@@ -1,40 +1,45 @@
 from Prisoner import Prisoner
 from Cupboard import Cupboard
 
-prisoners: [Prisoner] = [Prisoner(i) for i in range(100)]
-cupboard: Cupboard = Cupboard(100)
-
-
-def didAllPrisonersFoundLuckyCard(somePrisoners: [Prisoner]) -> bool:
-    return all([aPrisoner.isLuckyCardFound() for aPrisoner in somePrisoners])
+prisoners: [Prisoner] = [Prisoner(i) for i in range(10)]
+cupboard: Cupboard = Cupboard(10)
 
 
 def mkPrisLookForCardRandomly(
-    somePrisoners: [Prisoner], aCupboard: Cupboard
-) -> [Prisoner]:
+    somePrisoners: [Prisoner],
+    aCupboard: Cupboard,
+    noOfGuessesPerPrisoner: int = 50,
+) -> [bool]:
     prisonersCopy: [Prisoner] = somePrisoners[:]
     upRangeOfGuess: int = len(prisonersCopy) - 1
+    luckyCardsFound: [bool] = [False] * (upRangeOfGuess + 1)
     for aPrisoner in prisonersCopy:
         curGuess: int = 0
         uncoveredCardId: int = 0
-        for _ in range(50):
+        for _ in range(noOfGuessesPerPrisoner):
             curGuess = aPrisoner.getRandInt(0, upRangeOfGuess)
             uncoveredCardId = aCupboard.getCardIdFromDrawer(curGuess)
             if uncoveredCardId == aPrisoner.getId():
-                aPrisoner.setLuckyCardFound(True)
+                luckyCardsFound[aPrisoner.getId()] = True
                 break
-    return prisonersCopy
+    return luckyCardsFound
 
 
 def calcProbabilityOf100prisPardoned(
-    noOfIterations: int, prisList: [Prisoner], aCupboard: Cupboard
+    noOfIterations: int,
+    prisList: [Prisoner],
+    aCupboard: Cupboard,
+    noOfGuessesPerPrisoner: int = 50,
 ) -> float:
     allFoundLuckyCard: [bool] = [False] * noOfIterations
     for i in range(noOfIterations):
-        allFoundLuckyCard[i] = didAllPrisonersFoundLuckyCard(
-            mkPrisLookForCardRandomly(prisList, aCupboard)
+        allFoundLuckyCard[i] = all(
+            mkPrisLookForCardRandomly(
+                prisList, aCupboard, noOfGuessesPerPrisoner
+            )
         )
+
     return sum(allFoundLuckyCard) / noOfIterations
 
 
-print(calcProbabilityOf100prisPardoned(10000, prisoners, cupboard))
+print(calcProbabilityOf100prisPardoned(1000, prisoners, cupboard, 2))
