b063f2de95921677eca421147ae0f912c98fb803
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-11-23 20:30:25 +0100

    dodano funkcje uzywajaca sposobu metodycznego

--- part1_18_11_2021/task2/withouClasses/main.py
+++ part1_18_11_2021/task2/withouClasses/main.py
@@ -19,6 +19,25 @@ def didAllRandGuessesSucceded(
     return np.all(cardsFound == 1)
 
 
+def didAllMethodogicalGuessesSucceded(
+    cupboardSize: int = 10,
+    noOfPrisoners: int = 10,
+    guessesPerPrisoner: int = 8,
+) -> bool:
+    cardsFound: np.ndarray = np.zeros(noOfPrisoners)
+    cupboard: np.ndarray = np.arange(cupboardSize)
+    np.random.shuffle(cupboard)
+    for i in range(noOfPrisoners):  # for every prisoner
+        guess: int = i
+        for j in range(guessesPerPrisoner):
+            if cupboard[guess] == i:
+                cardsFound[i] = 1
+                break
+            else:
+                guess = cupboard[guess]
+    return np.all(cardsFound == 1)
+
+
 def calcProbability(fn, nIter: int = 2000) -> float:
     successes: [bool] = [fn() for _ in range(nIter)]
     return np.mean(successes)
