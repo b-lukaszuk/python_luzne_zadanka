c1375b85c6d5c2981388ea90ceb780dc65ef85fc
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-11-23 20:35:13 +0100

    kosmetyka, zam. 2 funkcji (dla randGuesses i methodicalGuesses) na 1

--- part1_18_11_2021/task2/withouClasses/main.py
+++ part1_18_11_2021/task2/withouClasses/main.py
@@ -2,42 +2,34 @@ import numpy as np
 import random
 
 
-def didAllRandGuessesSucceded(
+def didAllGuessesSucceded(
     cupboardSize: int = 10,
     noOfPrisoners: int = 10,
     guessesPerPrisoner: int = 8,
+    guessRandom: bool = True,
 ) -> bool:
+
     cardsFound: np.ndarray = np.zeros(noOfPrisoners)
     cupboard: np.ndarray = np.arange(cupboardSize)
     np.random.shuffle(cupboard)
+
     for i in range(noOfPrisoners):  # for every prisoner
-        for j in range(guessesPerPrisoner):
+        if guessRandom:
             guess: int = random.randint(0, cupboardSize - 1)  # incl, incl
-            if cupboard[guess] == i:
-                cardsFound[i] = 1
-                break
-    return np.all(cardsFound == 1)
-
-
-def didAllMethodogicalGuessesSucceded(
-    cupboardSize: int = 10,
-    noOfPrisoners: int = 10,
-    guessesPerPrisoner: int = 8,
-) -> bool:
-    cardsFound: np.ndarray = np.zeros(noOfPrisoners)
-    cupboard: np.ndarray = np.arange(cupboardSize)
-    np.random.shuffle(cupboard)
-    for i in range(noOfPrisoners):  # for every prisoner
-        guess: int = i
+        else:
+            guess: int = i
         for j in range(guessesPerPrisoner):
             if cupboard[guess] == i:
                 cardsFound[i] = 1
                 break
+            if guessRandom:
+                guess = random.randint(0, cupboardSize - 1)  # incl, incl
             else:
                 guess = cupboard[guess]
     return np.all(cardsFound == 1)
 
 
 def calcProbability(fn, nIter: int = 2000) -> float:
+
     successes: [bool] = [fn() for _ in range(nIter)]
     return np.mean(successes)
