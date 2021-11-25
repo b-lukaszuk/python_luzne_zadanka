1a22cca8113e25676c365d615161dc162a3762e2
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-11-25 09:29:01 +0100

    kosmetyka, zamiana 2 funkcji na 1 szukajaca lucky card

--- part1_18_11_2021/task2/withClasses/main.py
+++ part1_18_11_2021/task2/withClasses/main.py
@@ -5,68 +5,42 @@ prisoners: [Prisoner] = [Prisoner(i) for i in range(10)]
 cupboard: Cupboard = Cupboard(10)
 
 
-def mkPrisLookForCardRandomly(
+def mkPrisLookForLuckyCards(
     somePrisoners: [Prisoner],
     aCupboard: Cupboard,
-    noOfGuessesPerPrisoner: int = 50,
+    noOfGuessesPerPrisoner: int = 8,
+    searchRandomly: bool = True,
 ) -> [bool]:
-    prisonersCopy: [Prisoner] = somePrisoners[:]
-    upRangeOfGuess: int = len(prisonersCopy) - 1
-    luckyCardsFound: [bool] = [False] * (upRangeOfGuess + 1)
-    for aPrisoner in prisonersCopy:
-        curGuess: int = 0
-        uncoveredCardId: int = 0
-        for _ in range(noOfGuessesPerPrisoner):
-            curGuess = aPrisoner.getRandInt(0, upRangeOfGuess)
-            uncoveredCardId = aCupboard.getCardIdFromDrawer(curGuess)
-            if uncoveredCardId == aPrisoner.getId():
-                luckyCardsFound[aPrisoner.getId()] = True
-                break
-    return luckyCardsFound
 
-
-def mkPrisLookForCardMethodically(
-    somePrisoners: [Prisoner],
-    aCupboard: Cupboard,
-    noOfGuessesPerPrisoner: int = 50,
-):
     upRangeOfGuess: int = len(somePrisoners) - 1
     luckyCardsFound: [bool] = [False] * (upRangeOfGuess + 1)
-    for aPrisoner in somePrisoners:
-        curGuess: int = aPrisoner.getId()
-        uncoveredCardId: int = 0
+
+    for p in somePrisoners:
+
+        if searchRandomly:
+            curGuess: int = p.getRandInt(0, upRangeOfGuess)
+        else:
+            curGuess: int = p.getId()
+
         for _ in range(noOfGuessesPerPrisoner):
-            uncoveredCardId = aCupboard.getCardIdFromDrawer(curGuess)
-            if uncoveredCardId == aPrisoner.getId():
-                luckyCardsFound[aPrisoner.getId()] = True
+            uncoveredCardId: int = aCupboard.getCardIdFromDrawer(curGuess)
+            if uncoveredCardId == p.getId():
+                luckyCardsFound[p.getId()] = True
                 break
-            curGuess = uncoveredCardId
-    return luckyCardsFound
+            if searchRandomly:
+                curGuess = p.getRandInt(0, upRangeOfGuess)
+            else:
+                curGuess = uncoveredCardId
+    return luckyCardsFound  # lucky cards found or not
 
 
-def calcProbabilityOf100prisPardoned(
+def calcProbabilityOfAllPrisPardoned(
+    fn,
     noOfIterations: int,
-    prisList: [Prisoner],
-    aCupboard: Cupboard,
-    noOfGuessesPerPrisoner: int = 50,
-    searchRandomly: bool = True,
 ) -> float:
     allFoundLuckyCard: [bool] = [False] * noOfIterations
+
     for i in range(noOfIterations):
-        if searchRandomly:
-            allFoundLuckyCard[i] = all(
-                mkPrisLookForCardRandomly(
-                    prisList, aCupboard, noOfGuessesPerPrisoner
-                )
-            )
-        else:
-            allFoundLuckyCard[i] = all(
-                mkPrisLookForCardMethodically(
-                    prisList, aCupboard, noOfGuessesPerPrisoner
-                )
-            )
+        allFoundLuckyCard[i] = all(fn())
 
     return sum(allFoundLuckyCard) / noOfIterations
-
-
-print(calcProbabilityOf100prisPardoned(1000, prisoners, cupboard, 2))
