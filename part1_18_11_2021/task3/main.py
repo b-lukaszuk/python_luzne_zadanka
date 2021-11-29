11102e011dac85b04e24d373bd2cda18f8fc169c
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-11-29 21:51:10 +0100

    dodano plik main (z petla gry) do wywolania z Basha

---
+++ part1_18_11_2021/task3/main.py
@@ -0,0 +1,37 @@
+from Board import Board
+
+
+def printIntro():
+    print("Hello, let's play '15 puzzle game'\n")
+    print(
+        "Your task is to order the numbers from 1 to 15 (empty goes after 15)"
+    )
+    print("You can move one field at a time")
+    print("The field must be a neighbour to the empty field\n")
+    print("Let's begin :)")
+
+
+def main():
+    b: Board = Board()
+    printIntro()
+    while not b.isBoardSolved():
+        print()  # newline
+        print(b)
+        print("Enter Your move: 1-15: ")
+        try:
+            userChoice: int = int(input())
+            if not b.isMoveLegal(userChoice):
+                print("That's an illegal move try again.")
+            else:
+                b.makeMove(userChoice)
+                print("Move made")
+        except ValueError:
+            print("That was not a number")
+    print("\nThat's it. The puzzle is solved. You won. Congratulations!")
+    print("Behold the solved puzzle:")
+    print(b)
+    print("\nOK. That's it. Goodbye!")
+
+
+if __name__ == "__main__":
+    main()
