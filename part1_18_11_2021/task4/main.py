63358d13044853aeaaaeec7ca281b5a46a1b5743
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-01-07 18:51:39 +0100

    kosmetyka, poprawka otypowania

--- part1_18_11_2021/task4/main.py
+++ part1_18_11_2021/task4/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+from typing import List
 
 # based on:
 # https://www.youtube.com/watch?v=GuCzYxHa7iA
@@ -14,29 +15,29 @@ startBoard: np.ndarray = np.reshape(
 emptyField: int = 16
 
 
-def isAnyEltOutsideRange(aList: [int], lowIncl: int, upIncl: int) -> bool:
-    belowLow: [bool] = [i < lowIncl for i in aList]
-    aboveUp: [bool] = [i > upIncl for i in aList]
+def isAnyEltOutsideRange(aList: List[int], lowIncl: int, upIncl: int) -> bool:
+    belowLow: List[bool] = [i < lowIncl for i in aList]
+    aboveUp: List[bool] = [i > upIncl for i in aList]
     result = any(belowLow) or any(aboveUp)
     return result
 
 
 def removeEltsOutsideRange(
-    aList: [[int]], lowIncl: int, upIncl: int
-) -> [[int]]:
+    aList: List[List[int]], lowIncl: int, upIncl: int
+) -> List[List[int]]:
     return list(
         filter(lambda l: not isAnyEltOutsideRange(l, lowIncl, upIncl), aList)
     )
 
 
-def getLoc(numToFind: int, board: np.ndarray) -> [int, int]:
+def getLoc(numToFind: int, board: np.ndarray) -> List[int]:
     theLoc: np.ndarray = np.where(board == numToFind)
     return [theLoc[0][0], theLoc[1][0]]
 
 
-def getLegMovesLocs(arr2d: np.ndarray) -> [[int]]:
-    emptyLoc: [int, int] = getLoc(emptyField, arr2d)
-    neighLocs: [[int]] = []
+def getLegMovesLocs(arr2d: np.ndarray) -> List[List[int]]:
+    emptyLoc: List[int] = getLoc(emptyField, arr2d)
+    neighLocs: List[List[int]] = []
     nrow, ncol = emptyLoc
     neighLocs.extend([[nrow, ncol - 1], [nrow, ncol + 1]])
     neighLocs.extend([[nrow - 1, ncol], [nrow + 1, ncol]])
@@ -44,28 +45,28 @@ def getLegMovesLocs(arr2d: np.ndarray) -> [[int]]:
     return neighLocs
 
 
-def getLegMoves(arr2d: np.ndarray) -> [int]:
-    neighLocs = getLegMovesLocs(arr2d)
+def getLegMoves(arr2d: np.ndarray) -> List[int]:
+    neighLocs: List[List[int]] = getLegMovesLocs(arr2d)
     return [arr2d[r, c] for [r, c] in neighLocs]
 
 
 def getManhDistance(
     noOfInterest: int, curBoard: np.ndarray, solvedBoard: np.ndarray
 ) -> int:
-    cLoc: [int, int] = getLoc(noOfInterest, curBoard)
-    sLoc: [int, int] = getLoc(noOfInterest, solvedBoard)
+    cLoc: List[int] = getLoc(noOfInterest, curBoard)
+    sLoc: List[int] = getLoc(noOfInterest, solvedBoard)
     return abs(cLoc[0] - sLoc[0]) + abs(cLoc[1] - sLoc[1])
 
 
 def getLen(arr2d: np.ndarray) -> int:
-    dims: (int, int) = arr2d.shape
+    dims: List[int] = arr2d.shape
     return dims[0] * dims[1]
 
 
 # G cost - number of steps taken from initState to curState
 # H cost - number of steps taken from curState to goalState
 def calcCost(state1: np.ndarray, state2: np.ndarray) -> int:
-    costs: [int] = [0] * getLen(state1)
+    costs: List[int] = [0] * getLen(state1)
     for r in range(state1.shape[0]):
         for c in range(state1.shape[1]):
             costs[(r * c) + c] = getManhDistance(state1[r, c], state1, state2)
