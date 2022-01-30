6d8d2d0c405e1bb569fe8b054d078e201378463d
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-01-30 10:24:03 +0100

    dod. fn-s getSumPropDivs i areAmicablePair

--- part1_18_11_2021/task7/main.py
+++ part1_18_11_2021/task7/main.py
@@ -1,13 +1,25 @@
 from typing import List
 
 
-def getPropDiv(num: int) -> List[int]:
+def getPropDivs(num: int) -> List[int]:
     if num < 0:
         raise ValueError("Cannot compute proper divisors for values below 0")
     elif num == 0:
         return []
     else:
         return [i for i in range(1, num) if num % i == 0]
+
+
+def getSumPropDivs(num1: int) -> int:
+    return sum(getPropDivs(num1))
+
+
+def areAmicablePair(num1: int, num2: int) -> bool:
+    return (
+        (num1 != num2)
+        and (getSumPropDivs(num1) == num2)
+        and ((getSumPropDivs(num2) == num1))
+    )
 
 
 def main():
