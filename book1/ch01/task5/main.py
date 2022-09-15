2bc378258595202d1bbb87542b11e3254764d628
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-09-15 12:43:08 +0200

    improved error handling

--- book1/ch01/task5/main.py
+++ book1/ch01/task5/main.py
@@ -21,21 +21,38 @@ def isNumBetween(num: float, minIncl: float, maxIncl: float) -> bool:
     return (num >= minIncl) and (num <= maxIncl)
 
 
-def get_input_from_user(default_on_error: float = 3) -> List[float]:
+def get_input_from_user(
+    mins_of_inputs: List[float] = [0.1, 3],
+    maxes_of_inputs: List[float] = [1000, 100],
+    defaults_on_error: List[float] = [2, 3],
     messages: List[str] = [
-        "Enter the length [cm] (real number) of a side of a regular polygon: ",
-        "Enter the number of sides (integer) of a regular polygon: ",
-    ]
-    input_s_n: List[float] = [default_on_error, default_on_error]
+        "Enter the length [cm] (real number from: {0} to {1}) of a side of a regular polygon: ",
+        "Enter the number of sides (integer from {0} to {1}) of a regular polygon: ",
+    ],
+) -> List[float]:
+    """Returns user's input of side length and number of sides
+
+    Args:
+        mins_of_inputs (List[float]) - minimal values of side's length and number of sides
+        maxes_of_inputs (List[float]) - maximal values of side's length and number of sides
+        defaults_on_error (List[float]) - default values to return for side's length and number of sides if the input is wrong
+        messages (List[str]) - prompts to display while asking an user for input
+
+    Returns:
+        List[float] - length of a side and number of sides
+    """
+    input_s_n: List[float] = defaults_on_error.copy()
     for i in range(len(messages)):
-        user_input_str: str = input(messages[i])
+        user_input_str: str = input(
+            messages[i].format(mins_of_inputs[i], maxes_of_inputs[i])
+        )
         try:
             input_s_n[i] = float(user_input_str)
-            if not isNumBetween(input_s_n[i], 0.1, 100):
+            if not isNumBetween(input_s_n[i], mins_of_inputs[i], maxes_of_inputs[i]):
                 raise ValueError
         except ValueError:
-            print("Invalid input. I default to {0}".format(default_on_error))
-            input_s_n[i] = default_on_error
+            print("Invalid input. I default to {0}".format(defaults_on_error[i]))
+            input_s_n[i] = defaults_on_error[i]
     return input_s_n
 
 
@@ -55,7 +72,7 @@ def main() -> None:
 
     (s, n) = get_input_from_user()
     print(
-        "Calculating the area of a regular polygon with s = {s:.2f} [cm] and n = {n}.".format(
+        "\nCalculating the area of a regular polygon with s = {s:.2f} [cm] and n = {n}.".format(
             s=s, n=math.ceil(n)
         )
     )
