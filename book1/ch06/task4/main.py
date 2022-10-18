b622c4a6caf15eb50285e0bb56841362cf0c16ab
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-10-18 19:28:40 +0200

    added initial working solution to Task4

--- book1/ch06/task4/main.py
+++ book1/ch06/task4/main.py
@@ -1,28 +1,27 @@
 # -*- coding: utf-8 -*-
 
+import copy
 import random
 from typing import Any, Dict, List
 
 # returns dict with key columns identificator and digits (values) in List[int]
-# - 99 for middle of the card [2, 2]
+# middle of the card [2, 2] is empty (it is always considered to be marked)
 def get_digits_in_bingo() -> Dict[str, List[int]]:
     start: int = 1
     columns: Dict[str, List[int]] = {}
     for column in "bingo":
         random.seed(None)
         columns[column] = random.sample(population=range(start, start + 15), k=5)
-        # middle of a bingo card is empty (free space in the middle of the card)
         if column == "n":
-            columns[column][2] = -99
+            columns[column][2] = 0
         start += 15
     return columns
 
 
 def r_pad_entry(entry: Any, pad: str = " ", final_len: int = 3) -> str:
-    adjusted_entry: str = "  " if str(entry) == "-99" else str(entry)
     return "{0}{1}".format(
-        adjusted_entry,
-        pad * (final_len - len(str(adjusted_entry))),
+        entry,
+        pad * (final_len - len(str(entry))),
     )
 
 
@@ -60,11 +59,83 @@ def get_formatted_bingo(bingo: Dict[str, List[int]]) -> str:
     return result
 
 
+def mark_random_column(bingo: Dict[str, List[int]]) -> Dict[str, List[int]]:
+    random.seed(None)
+    col_id: str = random.choice(list("bingo"))
+    result: Dict[str, List[int]] = copy.deepcopy(bingo)
+    result[col_id] = [0] * 5
+    return result
+
+
+def mark_random_row(bingo: Dict[str, List[int]]) -> Dict[str, List[int]]:
+    result: Dict[str, List[int]] = copy.deepcopy(bingo)
+    random.seed(None)
+    row_id: int = random.choice(range(5))
+    for col_id in "bingo":
+        result[col_id][row_id] = 0
+    return result
+
+
+def mark_random_diagonal(bingo: Dict[str, List[int]]) -> Dict[str, List[int]]:
+    result: Dict[str, List[int]] = copy.deepcopy(bingo)
+    random.seed(None)
+    direction: List[int] = random.choice([list(range(0, 5)), list(range(4, -1, -1))])
+    col_ids: List[str] = list("bingo")
+    for i in range(len(col_ids)):
+        result[col_ids[i]][direction[i]] = 0
+    return result
+
+
+def get_sums_of_columns(bingo: Dict[str, List[int]]) -> List[int]:
+    return [sum(bingo[col_id]) for col_id in "bingo"]
+
+
+def is_any_col_marked(bingo: Dict[str, List[int]]) -> bool:
+    return any(elt == 0 for elt in get_sums_of_columns(bingo))
+
+
+def get_sums_of_rows(bingo: Dict[str, List[int]]) -> List[int]:
+    return [sum(get_row(bingo, row_id)) for row_id in range(5)]
+
+
+def is_any_row_marked(bingo: Dict[str, List[int]]) -> bool:
+    return any(elt == 0 for elt in get_sums_of_rows(bingo))
+
+
+def get_diagonal(bingo: Dict[str, List[int]], backslash: bool = True) -> List[int]:
+    direction: List[int] = list(range(0, 5)) if backslash else list(range(0, 5))[::-1]
+    col_ids: List[str] = list("bingo")
+    diagonal: List[int] = []
+    for i in range(len(col_ids)):
+        diagonal.append(bingo[col_ids[i]][direction[i]])
+    return diagonal
+
+
+def get_sums_of_diagonals(bingo: Dict[str, List[int]]) -> List[int]:
+    return [sum(get_diagonal(bingo)), sum(get_diagonal(bingo, False))]
+
+
+def is_any_diagonal_marked(bingo: Dict[str, List[int]]) -> bool:
+    return any(elt == 0 for elt in get_sums_of_diagonals(bingo))
+
+
+def is_bingo_winning(bingo: Dict[str, List[int]]) -> bool:
+    return (
+        is_any_col_marked(bingo)
+        or is_any_row_marked(bingo)
+        or is_any_diagonal_marked(bingo)
+    )
+
+
 def print_program_description() -> None:
     print("\nHi.\n")
     print("This is a toy program.")
-    print("It generates a random bingo card (American version).")
-    print("The result is printed on the screen.")
+    print("It checks if a bingo card (American version) is winning.")
+    print("By winning we mean either column, row or diagonal is all marked.")
+    print("The program generates random bingo and marks random fields.")
+    print("(random columns, rows, and diagonals)")
+    print("The program marks bingo fields with zeros.")
+    print("Then it performs the check.")
     print("NO GUARANTEE OF ITS ACCURACY. Still, I hope it'll work fine.")
     print("All clear. Then let's begin.")
 
@@ -72,7 +143,16 @@ def print_program_description() -> None:
 def main() -> None:
     print_program_description()
     bingo: Dict[str, List[int]] = get_digits_in_bingo()
-    print(get_formatted_bingo(bingo))
+    bingos: List[Dict[str, List[int]]] = [
+        bingo,
+        mark_random_column(bingo),
+        mark_random_row(bingo),
+        mark_random_diagonal(bingo),
+    ]
+    for i in random.sample(population=range(len(bingos)), k=len(bingos)):
+        print(get_formatted_bingo(bingos[i]))
+        print("Is winning? {0}".format(is_bingo_winning(bingos[i])))
+        print("\n\n========")
     print("That's all. Goodbye!\n")
 
 
