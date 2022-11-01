7e4154a7c76fac64d7a0faaa4f8d8530901a247e
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-11-01 20:50:58 +0100

    added program_description, main_program_loop, basically finished Task5 solution

--- book1/ch07/task5/main.py
+++ book1/ch07/task5/main.py
@@ -19,9 +19,7 @@ def add_values_to_elements(elements: Elements, line_from_csv: List[str]) -> None
     elements["atomicNumbers"].append(line_from_csv[0].strip())
 
 
-def get_elements_from_file(
-    file_path: str = "./PubChemElements_all.csv",
-) -> Elements:
+def get_elements_from_file(file_path: str) -> Elements:
     elements: Elements = {"fullNames": [], "symbols": [], "atomicNumbers": []}
     lines: List[str] = get_file_contents(file_path)
     for i in range(1, len(lines)):
@@ -55,7 +53,7 @@ def ask_user_for_action() -> str:
     input_collected: bool = False
     user_choice: str = ""
     keysDict: Dict = {"f": "fullNames", "s": "symbols", "a": "atomicNumbers"}
-    msg: str = "Action types:\n"
+    msg: str = "\nAvailable action types:\n"
     msg += "\ttype 'f': chose element by full name\n"
     msg += "\ttype 's': chose element by symbol\n"
     msg += "\ttype 'a': chose element by atomic number (# of protons)\n"
@@ -81,8 +79,40 @@ def ask_user_for_search_phrase(key_from_elements: str) -> str:
     return searched_phrase.strip()
 
 
+def print_program_description() -> None:
+    print("\nHi.\n")
+    print("This is a toy program searching through the elements periodic table.")
+    print("The table is based on publicly available PubChem data.")
+    print("You can search using full name, symbol or atomic number of an element.")
+    print("The result is printed to the terminal.")
+    print("NO GUARANTEE OF THE PROGRAM ACCURACY. Still, I hope it'll work fine.")
+    print("All clear. Then let's begin.\n")
+
+
+def program_main_loop(elements: Elements) -> None:
+    end_program: bool = False
+    key_from_elts: str = ""
+    searched_phrase: str = ""
+    elt_position: int = -99
+    while not end_program:
+        key_from_elts = ask_user_for_action()
+        searched_phrase = ask_user_for_search_phrase(key_from_elts)
+        elt_position = get_element_position(searched_phrase, key_from_elts, elements)
+        print(get_element_description(elt_position, elements))
+        end_program = input("\nWanna try again [y/n or anything else]? ") not in [
+            "y",
+            "Y",
+        ]
+
+
 def main() -> None:
-    print("Done. That's all. Goodbye!")
+    path_to_elements_data: str = "./PubChemElements_all.csv"
+    print_program_description()
+    print("Obtaining elements data from {0}".format(path_to_elements_data))
+    elements: Elements = get_elements_from_file(path_to_elements_data)
+    print("Done.\nObtaining user's input.")
+    program_main_loop(elements)
+    print("Done. That's all. Goodbye!\n")
 
 
 if __name__ == "__main__":
