460a36fbb240b65eabac012d6fb8099c1346661c
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-11-27 19:34:04 +0100

    cosmetic change

--- resource1/part1/ex_1_20/task3/main.py
+++ resource1/part1/ex_1_20/task3/main.py
@@ -4,7 +4,7 @@ from datetime import datetime as dt
 from typing import List, Tuple
 
 # date1, date2 example: 2022-11-27 (yyyy-mm-dd)
-def get_no_of_days_between(date1, date2) -> int:
+def get_no_of_days_between_dates(date1, date2) -> int:
     d1: dt = dt.strptime(date1, "%Y-%m-%d")
     d2: dt = dt.strptime(date2, "%Y-%m-%d")
     return abs((d2 - d1).days)
@@ -19,7 +19,7 @@ def main() -> None:
     ]
     for (date1, date2) in dates:
         print("\nCalculating number of days between {0} and {1}".format(date1, date2))
-        print("Result {0}".format(get_no_of_days_between(date1, date2)))
+        print("Result {0}".format(get_no_of_days_between_dates(date1, date2)))
     print("\nThat's all. Goodbye!")
 
 
