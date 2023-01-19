330bdd51efe8b3c3b2baad86d6595dcb2ababed4
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2023-01-19 20:50:38 +0100

    reduced the amount of code in existing functions

--- book3/p1/main.py
+++ book3/p1/main.py
@@ -2,41 +2,36 @@
 
 import random
 
+
 def get_rand_3dig_num() -> int:
     random.seed(a=None)
     return random.randint(100, 999)
 
+
 def is_bagles(guess: int, secret_num: int) -> bool:
-    g: str = str(guess)
-    sn: str = str(secret_num)
-    return not any([c in sn for c in g])
+    return not any([c in str(secret_num) for c in str(guess)])
+
 
 def get_except(text: str, ind: int) -> str:
-    result: str = ""
-    for i in range(len(text)):
-        if ind != i:
-            result = result+text[i]
-    return result
+    return "".join([c for (i, c) in enumerate(text) if i != ind])
+
 
 def is_pico(guess: int, secret_num: int) -> bool:
     g: str = str(guess)
     sn: str = str(secret_num)
-    for i in range(len(sn)):
-        if g[i] in get_except(sn, i):
-            return True
-    return False
+    return any([c in get_except(sn, i) for (i, c) in enumerate(g)])
+
 
 def is_fermi(guess: int, secret_num: int) -> bool:
     g: str = str(guess)
     sn: str = str(secret_num)
-    for i in range(len(sn)):
-        if g[i] == sn[i]:
-            return True
-    return False
+    return any([g[i] == sn[i] for i in range(len(sn))])
+
 
 def main() -> None:
     print("Project 1.")
     print("Status: Not finished.")
+
 
 if __name__ == "__main__":
     main()
