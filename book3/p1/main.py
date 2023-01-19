c9274631c6f3756b3cda55decea16b64aaa7584b
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2023-01-19 21:11:30 +0100

    added fn-s handling user's input

--- book3/p1/main.py
+++ book3/p1/main.py
@@ -28,6 +28,22 @@ def is_fermi(guess: int, secret_num: int) -> bool:
     return any([g[i] == sn[i] for i in range(len(sn))])
 
 
+def is_num_ok(guess: str) -> bool:
+    try:
+        num: int = int(guess)
+        return 99 < num < 1000
+    except ValueError:
+        return False
+
+
+def get_user_input() -> int:
+    guess: str = ""
+    while not is_num_ok(guess):
+        print("Enter your guess.")
+        guess = input("Enter 3 digit integer and press Enter: ")
+    return int(guess)
+
+
 def main() -> None:
     print("Project 1.")
     print("Status: Not finished.")
