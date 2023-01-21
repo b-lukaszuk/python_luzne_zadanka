785502003538ff4936362eeec3600b438087cca9
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2023-01-21 22:33:34 +0100

    corrected assertion in __init__() in Card, better __str__()

--- book3/p3/Card.py
+++ book3/p3/Card.py
@@ -9,9 +9,9 @@ class Card():
     # rank 2-10, 11-14 for: J, Q, K, A
     # suit: 0-3 for: clubs, diamonds, hearts, spades
     def __init__(self, rank: int, suit: int) -> None:
-        if 2 <= rank <= 14:
+        if not (2 <= rank <= 14):
             raise ValueError("The rank must be in range(2, 15)")
-        if 0 <= suit <= 3:
+        if not (0 <= suit <= 3):
             raise ValueError("The suit must be in range(4)")
         self.__rank__: int = rank
         self.__suit__: int = suit
@@ -24,7 +24,7 @@ class Card():
             return court_cards[self.__rank__]
 
     def __get_suit_representation__(self) -> str:
-        suits_symbols: Dict[int, str] = {0: "\u2663", 1: "\u2662", 2: "\u2661", 3: "\u2660"}
+        suits_symbols: Dict[int, str] = {0: "\u2663", 1: "\u2666", 2: "\u2665", 3: "\u2660"}
         return suits_symbols[self.__suit__]
 
     def __str__(self) -> str:
